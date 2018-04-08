ae4e597eb7a0bf8c4f9b54bea804562b748ecff4
wyk <85821307@qq.com>
2018-04-08 15:18:33 +0800

    Finish basic page

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//    return view('welcome');
+//});
+
+
+Route::get('/', 'PagesController@home')->name('home');
